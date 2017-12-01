51a77c40cd8d1ec8f37e4551d70fdd342edc52e5
rjshrjndrn <rajesh.r@optit.co>
2017-12-01 08:47:06 +0530

    bug fix

--- sourceable.sh
+++ sourceable.sh
@@ -1,12 +1,12 @@
 # theme
-source /home/rajesh/.oh-my-zsh/themes/robbyrussell.zsh-theme
+source ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 # oh-my-zsh plugins
-source /home/rajesh/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
-source /home/rajesh/.oh-my-zsh/plugins/z/z.plugin.zsh
-source /home/rajesh/.oh-my-zsh/plugins/git/git.plugin.zsh
-source /home/rajesh/.oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
-source /home/rajesh/.oh-my-zsh/plugins/docker-machine/docker-machine.plugin.zsh
-source /home/rajesh/.oh-my-zsh/plugins/golang/golang.plugin.zsh
-source /home/rajesh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-source /home/rajesh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-source /home/rajesh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source ~/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
+source ~/.oh-my-zsh/plugins/z/z.plugin.zsh
+source ~/.oh-my-zsh/plugins/git/git.plugin.zsh
+source ~/.oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
+source ~/.oh-my-zsh/plugins/docker-machine/docker-machine.plugin.zsh
+source ~/.oh-my-zsh/plugins/golang/golang.plugin.zsh
+source ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+source ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+source ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
