321c4399fb343c2031b486101a5a21c87454f31e
rjshrjndrn <rajesh.r@optit.co>
2017-12-01 10:40:18 +0530

    added missing source file

--- sourceable.sh
+++ sourceable.sh
@@ -7,6 +7,6 @@ source ~/.oh-my-zsh/plugins/git/git.plugin.zsh
 source ~/.oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
 source ~/.oh-my-zsh/plugins/docker-machine/docker-machine.plugin.zsh
 source ~/.oh-my-zsh/plugins/golang/golang.plugin.zsh
-source ~/.oh-my-zsh/plugins/history-substring-search/zsh-history-substring-search.zsh
+source ~/.oh-my-zsh/plugins/history-substring-search/history-substring-search.zsh
 source ~/.oh-my-zsh/custom/plugins/zsh-completions/zsh-completions.plugin.zsh
 source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
