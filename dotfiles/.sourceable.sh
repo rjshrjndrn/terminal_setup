a19bd9ea6cbbf8c7364701fdf46e266c71f1ea07
rjshrjndrn <rjshrjndrn@gmail.com>
2018-01-12 21:02:13 +0530

    updated sourceable for docker

--- dotfiles/.sourceable.sh
+++ dotfiles/.sourceable.sh
@@ -1,6 +1,9 @@
 # theme
 source ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 # oh-my-zsh plugins
+source /home/rajesh/.oh-my-zsh/plugins/docker-machine/docker-machine.plugin.zsh
+source /home/rajesh/.oh-my-zsh/plugins/docker-compose/_docker-compose
+source /home/rajesh/.oh-my-zsh/plugins/docker/_docker
 source ~/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
 source ~/.oh-my-zsh/plugins/z/z.plugin.zsh
 source ~/.oh-my-zsh/plugins/git/git.plugin.zsh
@@ -12,4 +15,4 @@ source ~/.oh-my-zsh/custom/plugins/zsh-completions/zsh-completions.plugin.zsh
 
 # This plugin is not compatible zsh older than 1.11 which shipped with ubuntu 14.04
 # From 16.04, it'll be okay
-#source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
