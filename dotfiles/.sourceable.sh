7398e8da6113fe8ff7bfe6cf00132baf109dd808
rjshrjndrn <rjshrjndrn@gmail.com>
2018-01-27 11:31:42 +0530

    changed theme

--- dotfiles/.sourceable.sh
+++ dotfiles/.sourceable.sh
@@ -1,5 +1,5 @@
 # theme
-source ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
+source ~/.oh-my-zsh/themes/amuse.zsh-theme
 # oh-my-zsh plugins
 source ~/.oh-my-zsh/plugins/docker-machine/docker-machine.plugin.zsh
 source ~/.oh-my-zsh/plugins/docker-compose/_docker-compose
