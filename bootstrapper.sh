373890335775acce1c86fef01a76b9286fde9757
rjshrjndrn <rjshrjndrn@gmail.com>
2018-04-12 12:35:25 +0530

    Issue #000 fix: removed copying

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -22,7 +22,6 @@ sudo apt install vim-gtk -y
 
 # Copying configurations
 find $(pwd)/dotfiles -type f -exec ln -sf {} ~/ \;
-cp -rf dotfiles/. ~/
 
 # Cloning Vundle 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
